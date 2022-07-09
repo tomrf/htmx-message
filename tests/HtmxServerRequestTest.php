cc7de54d91873ba9e5c5485eea0397303862f0ee
Tom Rune Flo <tom@upnode.no>
2022-07-10 00:50:20 +0200

    update tests

--- tests/HtmxServerRequestTest.php
+++ tests/HtmxServerRequestTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Tomrf\HtmxMessage\Test;
 
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
 use Tomrf\HtmxMessage\HtmxServerRequest;
@@ -17,43 +19,60 @@ use Tomrf\HtmxMessage\HtmxServerRequest;
  */
 final class HtmxServerRequestTest extends TestCase
 {
+    static private Psr17Factory $psr17Factory;
+    static private ServerRequestCreator $serverRequestCreator;
+
     public static function setUpBeforeClass(): void
     {
-        // ...
+        static::$psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+        static::$serverRequestCreator  = new \Nyholm\Psr7Server\ServerRequestCreator(
+           static::$psr17Factory,
+           static::$psr17Factory,
+           static::$psr17Factory,
+           static::$psr17Factory
+        );
+    }
+
+    private static function createServerRequest(string $method = 'GET', string $uri = '/', ?string $body = null): ServerRequestInterface
+    {
+        return static::$serverRequestCreator->fromArrays(
+            [
+                'REQUEST_METHOD' => $method,
+                'REQUEST_URI' => $uri,
+                'SERVER_PROTOCOL' => '1.1',
+                'SERVER_NAME' => 'localhost',
+                'SERVER_PORT' => '80',
+                'REMOTE_ADDR' => '::1',
+                'REMOTE_PORT' => '58981',
+                'SERVER_ADDR' => '::1',
+            ],
+            [
+                'HX-Request' => 'true',
+                'Accept' => 'text/html',
+                'Accept-Charset' => 'utf-8',
+                'Accept-Language' => 'en-US,en;q=0.9',
+                'Cache-Control' => 'no-cache',
+                'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+            ],
+            $_COOKIE,
+            $_GET,
+            $_POST,
+            $_FILES,
+            $body
+
+        );
     }
 
     public function testNewInstanceFromServerRequest(): void
     {
-        $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
-
-        $creator = new \Nyholm\Psr7Server\ServerRequestCreator(
-            $psr17Factory,
-            $psr17Factory,
-            $psr17Factory,
-            $psr17Factory
-        );
-
-        $serverRequest = $creator->fromArrays(
-            [
-                'HTTP_HOST' => 'example.com',
-                'REQUEST_URI' => '/',
-                'REQUEST_METHOD' => 'GET',
-            ],
-            [
-                'host' => 'example.com',
-                'accept' => 'text/html',
-                'hx-request' => 'true',
-            ],
-            [],
-            [],
-            [],
-            [],
-            ''
-        );
+        $serverRequest = static::createServerRequest();
         $htmxRequest = new HtmxServerRequest($serverRequest);
 
         static::assertInstanceOf(ServerRequestInterface::class, $serverRequest);
         static::assertInstanceOf(ServerRequestInterface::class, $htmxRequest);
+
+        // assert isHxRequest
+        static::assertTrue($htmxRequest->isHxRequest());
 
         // assert request method
         static::assertSame('GET', $serverRequest->getMethod());
@@ -63,53 +82,28 @@ final class HtmxServerRequestTest extends TestCase
         static::assertSame('/', $serverRequest->getUri()->getPath());
         static::assertSame($serverRequest->getUri(), $htmxRequest->getUri());
 
-        // assert request host header
-        static::assertSame('example.com', $serverRequest->getHeader('Host')[0]);
-        static::assertSame($serverRequest->getHeader('Host'), $htmxRequest->getHeader('Host'));
-
         // assert protocol version change
         static::assertSame('1.1', $htmxRequest->getProtocolVersion());
         $newRequest = $htmxRequest->withProtocolVersion('1.0');
         static::assertSame('1.0', $newRequest->getProtocolVersion());
         static::assertNotSame($htmxRequest, $newRequest);
 
-        // assert manipulating body and adding attribute
-        static::assertSame('', $htmxRequest->getBody()->getContents());
+        // assert adding attribute
         $newRequest = $htmxRequest->withAttribute('attr', 123);
+        static::assertSame(123, $newRequest->getAttribute('attr'));
+        static::assertNotSame($htmxRequest, $newRequest);
+
+        // assert manipulating body content
         $newRequest->getBody()->write('foo');
         $newRequest->getBody()->rewind();
         static::assertSame('foo', $newRequest->getBody()->getContents());
-        static::assertSame(123, $newRequest->getAttribute('attr'));
-        static::assertNotSame($htmxRequest, $newRequest);
     }
 
     public function testNewInstanceFromNonHtmxRequest(): void
     {
-        $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
-
-        $creator = new \Nyholm\Psr7Server\ServerRequestCreator(
-            $psr17Factory,
-            $psr17Factory,
-            $psr17Factory,
-            $psr17Factory
+        static::assertFalse(
+            (new HtmxServerRequest(static::createServerRequest()->withoutHeader('HX-Request')))
+                ->isHxRequest()
         );
-
-        $serverRequest = $creator->fromArrays(
-            [
-                'HTTP_HOST' => 'example.com',
-                'REQUEST_URI' => '/',
-                'REQUEST_METHOD' => 'GET',
-            ],
-            [],
-            [],
-            [],
-            [],
-            [],
-            ''
-        );
-
-        $htmxRequest = new HtmxServerRequest($serverRequest);
-
-        static::assertFalse($htmxRequest->isHxRequest());
     }
 }
